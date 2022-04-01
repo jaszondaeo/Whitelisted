a273a453ba319d32de3175bfe0385bedb2967df4
Cadez650 <67844200+Cadez650@users.noreply.github.com>
2022-04-01 11:59:47 -0700

    Update hwids.lua

--- hwids.lua
+++ hwids.lua
@@ -1,5 +1,5 @@
-return function()
-    return {
-        "b6c1542212bb9630d8ea44213826cc019970b70574c3e57b2d189f7e79bfcfb2c060ef663de8e2b2d3d648f0526b0559675a22e56c7556734f9cbbf5a6d1f4c6"
-    }
-end
+local whitelisted = {
+    "b6c1542212bb9630d8ea44213826cc019970b70574c3e57b2d189f7e79bfcfb2c060ef663de8e2b2d3d648f0526b0559675a22e56c7556734f9cbbf5a6d1f4c6"
+}
+
+return whitelisted
